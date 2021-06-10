9bcb9d82dcea415f2ea6d8856f49fa080b7a93a1
maayan buzaglo <maayanbuzaglo@h-MacBook-Pro-sl-maayan.local>
2021-06-10 12:47:08 +0300

    Invalid mail works

--- 2.landing-page-vanila/landingPage.js
+++ 2.landing-page-vanila/landingPage.js
@@ -1,16 +1,15 @@
-function validateEmail() {
+function validateEmail(e) {
+    e.preventDefault();
     var emailInput = document.form1.email.value;
     var validRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (emailInput.match(validRegex)){
       alert("We sent the details to your email\n Have a nice day :)");
-    //   document.form1.email.focus();
-      document.getElementById("invalid-mail").innerHTML = "";
-      document.getElementById("email-input").innerHTML = "";
+      location.reload();
     }
     else
     {
-        var invalidMail = document.getElementById("invalid-mail").innerHTML = "mmm";
-        // document.form1.email.focus();
-    //   return false;
+        document.getElementById("email-input").innerHTML = "";
+        var invalidMail = document.getElementById("invalid-mail").innerHTML = "Invalid email. please try again";
+        document.form1.email.focus();
     }
   }
