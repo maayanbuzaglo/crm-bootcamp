c8a2a9867729e9d86ba68b44576496d3088ccf1b
maayan buzaglo <maayanbuzaglo@h-MacBook-Pro-sl-maayan.local>
2021-06-10 14:22:18 +0300

    Added comments

--- 2.landing-page-vanila/landingPage.js
+++ 2.landing-page-vanila/landingPage.js
@@ -1,11 +1,14 @@
 function validateEmail(e) {
-    e.preventDefault();
-    var emailInput = document.form1.email.value;
-    var validRegex = /^([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)@(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})$/;
+    e.preventDefault(); //for prevent the reload of page.
+    var emailInput = document.form1.email.value; //gets the email.
+    var validRegex = /^([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)@(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})$/; //email Regex.
+
+    //If email correct - alert.
     if (emailInput.match(validRegex)){
         alert("We sent the details to your email\n Have a nice day :)");
         location.reload();
     }
+    //else - invalid mail text.
     else
     {
         document.getElementById("email-input").innerHTML = "";
