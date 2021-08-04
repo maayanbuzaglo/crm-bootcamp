c49f4b2a54b60a627e39e85ba3d6c7203e6e76e9
maayan buzaglo <maayanbuzaglo@h-MacBook-Pro-sl-maayan.local>
2021-08-04 19:23:45 +0300

    Landing page chat conversation saved on reload, still have problem with time

--- 3.landing-page-node/views/js/chat.js
+++ 3.landing-page-node/views/js/chat.js
@@ -3,29 +3,105 @@ var messages = document.getElementById("messages");
 var form = document.getElementById("chat-form");
 var input = document.getElementById("chat-input");
 var typing = document.getElementById("typing");
-let firstMessage = true;
-let id = "";
 
-const onRender = async () => {
+let id = "";
+let conversation = [];
+
+/*
+ This function creates a message on the chat.
+*/
+const createMessage = (tmpMessage) => {
+  //If message send to me.
+  if (!tmpMessage.isCrmSender) {
+    var div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.margin = "10px";
+
+    var item = document.createElement("div");
+    item.textContent = tmpMessage.message;
+    item.style.color = "white";
+    item.style.backgroundColor = "#002c9b";
+    item.style.padding = "10px";
+    item.style.borderRadius = "10px";
+
+    var date = document.createElement("div");
+    var dateTmp = new Date(tmpMessage.date);
+    date.textContent =
+      tmpMessage.date.split(" ")[1] +
+      " " +
+      tmpMessage.date.split(" ")[2] +
+      " " +
+      dateTmp.getHours() +
+      ":" +
+      dateTmp.getMinutes();
+    date.style.color = "grey";
+    date.style.fontSize = "10px";
+    date.style.marginLeft = "10px";
+    date.style.marginRight = "10px";
+
+    div.appendChild(item);
+    div.appendChild(date);
+    messages.appendChild(div);
+    //If I send the message.
+  } else {
+    typing.style.display = "none";
+
+    var div = document.createElement("div");
+    div.style.display = "flex";
+    div.style.justifyContent = "flex-end";
+    div.style.margin = "10px";
+
+    var item = document.createElement("div");
+    item.textContent = tmpMessage.message;
+    item.style.color = "white";
+    item.style.backgroundColor = "grey";
+    item.style.padding = "10px";
+    item.style.borderRadius = "10px";
+
+    var date = document.createElement("div");
+    var dateTmp = new Date(tmpMessage.date);
+    date.textContent =
+      tmpMessage.date.split(" ")[1] +
+      " " +
+      tmpMessage.date.split(" ")[2] +
+      " " +
+      dateTmp.getHours() +
+      ":" +
+      dateTmp.getMinutes();
+    date.style.color = "grey";
+    date.style.fontSize = "10px";
+    date.style.marginLeft = "10px";
+    date.style.marginRight = "10px";
+
+    div.appendChild(date);
+    div.appendChild(item);
+    messages.appendChild(div);
+  }
+};
+
+const onRender = () => {
   const room = window.localStorage.getItem("room");
-  // axios
-  //   .post("http://localhost:9090/chat/clientMessages", {
-  //     email: room,
-  //   })
-  //   .then(function (response) {
-  //     console.log(response);
-  //   })
-  //   .catch(function () {});
+  axios
+    .post("http://localhost:9090/chat/clientMessages", {
+      email: room,
+    })
+    .then(function (response) {
+      conversation = response.data.messages;
+      for (message in conversation) {
+        tmpMessage = conversation[message];
+        createMessage(tmpMessage);
+      }
+    })
+    .catch(function () {});
 };
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
-    if (firstMessage) {
-      id = input.value;
-      firstMessage = false;
+    if (!window.localStorage.getItem("room")) {
       window.localStorage.setItem("room", input.value);
     }
+    id = window.localStorage.getItem("room");
     socket.emit("join-room", id);
     socket.emit("send-message", input.value, id);
     input.value = "";
@@ -52,6 +128,7 @@ div.appendChild(item);
 messages.appendChild(div);
 
 socket.on("receive-message", (msg) => {
+  console.log(msg);
   //If message send to me.
   if (!msg.isCrmSender) {
     var div = document.createElement("div");
@@ -63,7 +140,23 @@ socket.on("receive-message", (msg) => {
     item.style.backgroundColor = "#002c9b";
     item.style.padding = "10px";
     item.style.borderRadius = "10px";
+    var date = document.createElement("div");
+    // var dateTmp = Date.now();
+    date.textContent = Date.now();
+    // dateTmp.split(" ")[1] +
+    //   " " +
+    //   dateTmp.split(" ")[2] +
+    //   " " +
+    //   dateTmp.getHours() +
+    //   ":" +
+    //   dateTmp.getMinutes();
+    date.style.color = "grey";
+    date.style.fontSize = "10px";
+    date.style.marginLeft = "10px";
+    date.style.marginRight = "10px";
+
     div.appendChild(item);
+    div.appendChild(date);
     messages.appendChild(div);
     //If I send the message.
   } else {
@@ -78,6 +171,22 @@ socket.on("receive-message", (msg) => {
     item.style.backgroundColor = "grey";
     item.style.padding = "10px";
     item.style.borderRadius = "10px";
+    var date = document.createElement("div");
+    // var dateTmp = Date.now();
+    date.textContent = Date.now();
+    // msg.date.split(" ")[1] +
+    // " " +
+    // msg.date.split(" ")[2] +
+    // " " +
+    // dateTmp.getHours() +
+    // ":" +
+    // dateTmp.getMinutes();
+    date.style.color = "grey";
+    date.style.fontSize = "10px";
+    date.style.marginLeft = "10px";
+    date.style.marginRight = "10px";
+
+    div.appendChild(date);
     div.appendChild(item);
     messages.appendChild(div);
   }
